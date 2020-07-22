9e436323f3079abe3dd036e91068d97ae440c5d0
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2020-07-21 20:53:07 -0500

    Artist, Album and Song classes were putted in Django andmin

--- apps/collector/admin.py
+++ apps/collector/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    ordering = ('name', 'popularity', 'followers')
+    search_fields = ('name', 'id')
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    ordering = ('name', 'popularity', 'artist')
+    search_fields = ('name', 'artist', 'id')
+
+
+class SongAdmin(admin.ModelAdmin):
+    ordering = ('name', 'album', 'popularity', 'energy', 'tempo', 'valence')
+    search_fields = ('name', 'album', 'id')
+
+admin.site.register(models.Artist, ArtistAdmin)
+admin.site.register(models.Album, AlbumAdmin)
+admin.site.register(models.Song, SongAdmin)
