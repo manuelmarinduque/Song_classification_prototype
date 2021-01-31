4dc3cfbb2554b8531939f2b025db1df02ad1a9b1
Manuel Marin Duque <manuel.duque5.2@gmail.com>
2021-01-31 18:22:49 -0500

    Se añade un TODO sobre emplear una CBV distinta, en la cual se sobreescriba el método get_success_url().

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('playlist/', GeneratePlaylistView.as_view(), name='playlist_page'),
     # TODO Quizás pueda funcionar el framework 'messages' de Django, el cual envía mensajes de la vista al template. Leer la documentación.
     # TODO Quizás se pueda emplear un FBV como se hace en la app 'collector'.
+    # TODO Quizás se pueda emplear una CBV distinta, en la cual se sobreescriba el método get_success_url(), como se hace en el proyecto web_playground.
     path('playlist/<confirmation>', GeneratePlaylistView.as_view(), name='playlist_confirmation_page'),
     path('create_playlist/<playlist_value>/', CreateSelectedPlaylist.as_view(), name='create_playlist_page'),
 ]
