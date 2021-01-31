cf31833ac3e320c83cc1a9f5b89d95163a07a400
Manuel Marin Duque <manuel.duque5.2@gmail.com>
2021-01-31 18:19:17 -0500

    Se añade un TODO sobre emplear un FBV como se hace en la app 'collector'.

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('home/', HomeView.as_view(), name='home_page'),
     path('playlist/', GeneratePlaylistView.as_view(), name='playlist_page'),
     # TODO Quizás pueda funcionar el framework 'messages' de Django, el cual envía mensajes de la vista al template. Leer la documentación.
+    # TODO Quizás se pueda emplear un FBV como se hace en la app 'collector'.
     path('playlist/<confirmation>', GeneratePlaylistView.as_view(), name='playlist_confirmation_page'),
     path('create_playlist/<playlist_value>/', CreateSelectedPlaylist.as_view(), name='create_playlist_page'),
 ]
