8a2260dabece694d9705b648e86cceb48920dce5
Manuel Marin Duque <manuel.duque5.2@gmail.com>
2021-01-31 18:00:10 -0500

    Debido a los problemas que trae utilizar una variable global para guardar el resultado de la clasificación de las canciones de un usuario,  problemas como si el servidor se reinicia la variable también y si se limpia la caché la variable también se reinicia, se decide generar un csv con la clasificación de las canciones y luego se lee este csv para generar la playlist seleccionada.

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,6 +4,8 @@ from social_django.models import UserSocialAuth
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
+import pandas as pd
+
 # Create your views here.
 
 
@@ -12,7 +14,9 @@ from django.urls import reverse_lazy
 # al crear la playlist en la cuenta del usuario.
 # El servidor no se puede reiniciar porque también reinicia a 'None' el valor de la variable global,
 # lo cual lo hace susceptible a fallos. Si se reinicia se deben generar las playlist nuevamente.
-data_frame_global = None
+# También sucede lo mismo que lo anterior si se refresca la página limpiando la caché, es decir,
+# si se refresca mediante ctrl+f5.
+# dataframe_global = None
 
 
 class LoginView(TemplateView):
@@ -34,8 +38,7 @@ class GeneratePlaylistView(LoginRequiredMixin, TemplateView):
         collector = Collector('null', token, request.user)
         data_frame = collector.readSavedTracks()
         data_frame_pred = collector.modelPredicts(data_frame)
-        global data_frame_global
-        data_frame_global = data_frame_pred
+        data_frame_pred.to_csv('apps/core/data/data_frame_pred.csv', index=False)
         data_frame_pred = data_frame_pred.drop(['track_id', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'loudness', 'tempo', 'valence', 'liveness', 'speechiness'], axis=1)
         self.extra_context = {'relax_songs': data_frame_pred[data_frame_pred.emotion == 0].values,
                               'happy_songs': data_frame_pred[data_frame_pred.emotion == 1].values,
@@ -51,14 +54,14 @@ class CreateSelectedPlaylist(LoginRequiredMixin, RedirectView):
         token = UserSocialAuth.objects.get(user=request.user.id).extra_data.get('access_token')
         collector = Collector('null', token, request.user)
         playlist_value = self.kwargs.get('playlist_value')
-        global data_frame_global
+        dataframe = pd.read_csv('apps/core/data/data_frame_pred.csv')
         if playlist_value == '1':
-            collector.createPlaylist('Momentos felices', data_frame_global[data_frame_global.emotion == 1].track_id.values)
-            print(len(data_frame_global[data_frame_global.emotion == 1].track_id.values))
+            collector.createPlaylist('Momentos felices', dataframe[dataframe.emotion == 1].track_id.values)
+            print(len(dataframe[dataframe.emotion == 1].track_id.values))
         elif playlist_value == '2':
-            collector.createPlaylist('Momentos de relajación', data_frame_global[data_frame_global.emotion == 0].track_id.values)
-            print(len(data_frame_global[data_frame_global.emotion == 0].track_id.values))
+            collector.createPlaylist('Momentos de relajación', dataframe[dataframe.emotion == 0].track_id.values)
+            print(len(dataframe[dataframe.emotion == 0].track_id.values))
         elif playlist_value == '3':
-            collector.createPlaylist('Momentos tristes', data_frame_global[data_frame_global.emotion == 2].track_id.values)
-            print(len(data_frame_global[data_frame_global.emotion == 2].track_id.values))
+            collector.createPlaylist('Momentos tristes', dataframe[dataframe.emotion == 2].track_id.values)
+            print(len(dataframe[dataframe.emotion == 2].track_id.values))
         return super().get(request, *args, **kwargs)
